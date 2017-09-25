93a822e8837049bac22bcead7841b5cac3f4425d
Krittiya Clark <krittiyaclark@gmail.com>
2017-09-25 18:17:14 -0400

    Fetch Youtube data success

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBOa5J9NYPYuxlhSshs3s8H04VBQOtvgEU';
 
-const API_KEY = 'AIzaSyCeazBo3HnieAROb3kB-Yw6vR6X8QyoFGw';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { videos: [] };
 
-const App = () => {
-  return (
-           <div>
-             <SearchBar />
-           </div>
-         )
-};
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos }); // ES6 key and value are the same videos: videos
+    });
+  }
+
+  render() {
+    return (
+       <div>
+         <SearchBar />
+         <VideoList videos={this.state.videos} />
+       </div>
+    );
+  }
+}
 
 ReactDom.render(<App />, document.querySelector('.container'))
